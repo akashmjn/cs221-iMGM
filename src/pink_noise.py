f0e8a55483ca1ef83a5d459a734039e41f163aa0
Suraj <hrsuraj@stanford.edu>
2017-11-18 04:00:10 -0800

    working baseline

--- src/pink_noise.py
+++ src/pink_noise.py
@@ -1,18 +1,28 @@
 import numpy as np
 
+from midi import matrix_to_midi
+
 ###############################################################################
 
 class PinkNoise:
+    def __init__(self, num_notes=100):
+        self.n = num_notes
+        frequencies = [261.63]
+        for i in range(35):
+            frequencies.append(frequencies[-1]*1.06)
+        probabilities = [1/f for f in frequencies]
+        sump = sum(probabilities)
+        self.prob = [p/sump for p in probabilities]
+        self.notes = list(range(48,84))
     
+    def generate(self, output_path):
+        notes = []
+        for _ in range(self.n):
+            note = np.random.choice(self.notes, p=self.prob)
+            row = np.zeros(128,)
+            row[note] = 1
+            notes.append(row)
+        notes = np.vstack(notes)
+        matrix_to_midi(matrix=notes, out_midi_path=output_path)
 
-notes = list(range(128))
-
-def pink_noise(num_notes=100):
-    frequencies = [16.35]
-    for i in range(127):
-        frequencies.append(frequencies[-1]*1.06)
-    
-    probabilities = [1/f for f in frequencies]
-    sump = sum(probabilities)
-    prob = [p/sump for p in probabilities]
-    
+###############################################################################
