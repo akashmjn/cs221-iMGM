0f424cb7ceffb7f688e92b2d55026a29b774028c
Nathan Dalal <nathan@DN0a232205.SUNet>
2017-11-17 17:42:08 -0800

    committing a trial run of recursive function -- not tested

--- src/midi.py
+++ src/midi.py
@@ -16,22 +16,29 @@ NUM_POSSIBLE_NOTES = 128
 '''
 Calls merge_tracks() on each MIDI file in input_dir and places into output_dir.
 
-This function has no return value and puts the MIDI files 
-  in the output path specified.
-
 @param input_dir -> path to folder containing MIDI file to convert
 @param out_midi_path -> path to folder which will contain outputted MIDI files
+@param recursive -> whether to recursively search through directories
 @param out_instrument_name -> instrument of the output file track
 @param include_drums -> allows filtering out of drum and percussion based instruments
+@param num_tracks -> should be set to zero, do not change -> important for labeling files in recursion
+
+@return number of tracks merged
 '''
-def merge_many_tracks(input_dir, output_dir, merged_filename_label = "merged" ,out_instrument_name = "Cello", include_drums = True):
+def merge_many_tracks(input_dir, output_dir, recursive = True, merged_filename_label = "merged" ,out_instrument_name = "Cello", include_drums = True, num_tracks = 0):
     import os
     for root, dirs, filenames in os.walk(input_dir):
         for f in filenames:
             if f.endswith(".mid"):
                 input_file_path = os.path.join(input_dir, f)
-                output_file_path = os.path.join(output_dir, f[:-4] + "_" + merged_filename_label + ".mid")
+                output_file_path = os.path.join(output_dir, str(num_tracks) + "_" + f[:-4] + "_" + merged_filename_label + ".mid")
                 merge_tracks(input_file_path, output_file_path, out_instrument_name, include_drums)
+                num_tracks += 1
+        if recursive:
+            for next_dirname in dirs:
+                num_tracks += merge_many_tracks(os.path.join(input_dir, next_dirname), output_dir, out_instrument_name, include_drums)
+
+    return num_tracks
 
 '''
 Merges all tracks of one MIDI file and outputs a MIDI file with just one track.
